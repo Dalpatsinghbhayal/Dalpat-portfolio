faa2dd1db2363cb1568f24d567e2c423539e4616
Dalpatsinghbhayal <dalpatbhayal07@gmail.com>
2026-08-08 14:34:32 +0530

    Update projects section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -44,7 +44,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1w7M4aWvqXs01KXLhA2K8yp3VIRVx46Qx/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1k1KsYUJvJdZZ81aoDnW_ytc90hrpuUPT/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
